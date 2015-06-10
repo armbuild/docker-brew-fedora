20e0aa70855bf6e9d4f28f5cb8ff6ad31c17231c
Manfred Touron <m@42.am>
2015-06-10 16:37:50 +0200

    Bumped to 22 (Fixes #2)

--- buildcontainers.sh
+++ buildcontainers.sh
@@ -3,34 +3,34 @@
 set -xe
 
 # Fetching & uncompress release
-wget http://fedora.mirrors.ovh.net/linux/releases/21/Images/armhfp/Fedora-Minimal-armhfp-21-5-sda.raw.xz
-unxz -v Fedora-Minimal-armhfp-21-5-sda.raw.xz
+wget http://fedora.mirrors.ovh.net/linux/releases/22/Images/armhfp/Fedora-Minimal-armhfp-22-3-sda.raw.xz
+unxz -v Fedora-Minimal-armhfp-22-3-sda.raw.xz
 
 # Mounting the rootfs in loop
-#    root@fwrz:~/docker-brew-fedora# fdisk -lu Fedora-Minimal-armhfp-21-5-sda.raw
+#    root@7f8f5e3c610b:/# fdisk -lu /host/Fedora-Minimal-armhfp-22-3-sda.raw
 #
-#    Disk Fedora-Minimal-armhfp-21-5-sda.raw: 2139 MB, 2139095040 bytes
-#    255 heads, 63 sectors/track, 260 cylinders, total 4177920 sectors
+#    Disk /host/Fedora-Minimal-armhfp-22-3-sda.raw: 1841 MB, 1841299456 bytes
+#    255 heads, 63 sectors/track, 223 cylinders, total 3596288 sectors
 #    Units = sectors of 1 * 512 = 512 bytes
 #    Sector size (logical/physical): 512 bytes / 512 bytes
 #    I/O size (minimum/optimal): 512 bytes / 512 bytes
-#    Disk identifier: 0xa99d2bd5
+#    Disk identifier: 0xbcdd5210
 #
-#                             Device Boot      Start         End      Blocks   Id  System
-#    Fedora-Minimal-armhfp-21-5-sda.raw1            2048     1001471      499712   83  Linux
-#    Fedora-Minimal-armhfp-21-5-sda.raw2         1001472     1251327      124928   83  Linux
-#    Fedora-Minimal-armhfp-21-5-sda.raw3         1251328     3985407     1367040   83  Linux
-# The first partition is /boot, the second partition is swap, the third partition is the rootfs (what we want)
+#    Device Boot      Start         End      Blocks   Id  System
+#    /host/Fedora-Minimal-armhfp-22-3-sda.raw1            2048      587775      292864   83  Linux
+#    /host/Fedora-Minimal-armhfp-22-3-sda.raw2          587776     1087487      249856   83  Linux
+#    /host/Fedora-Minimal-armhfp-22-3-sda.raw3         1087488     3432447     1172480   83  Linux
+
 rm -rf rootfs && mkdir rootfs
-mount -o loop,offset=$((1251328 * 512)) Fedora-Minimal-armhfp-21-5-sda.raw rootfs
+mount -o loop,offset=$((1087488 * 512)) Fedora-Minimal-armhfp-22-3-sda.raw rootfs
 
 # make clean ?
 
-tar -C rootfs/ -jcf fedora-21-minimal.tar.xz .
+tar -C rootfs/ -jcf fedora-22-minimal.tar.xz .
 
 umount rootfs
 
-docker build -t armbuild/fedora-qcow-minimal:21 .
-docker tag armbuild/fedora-qcow-minimal:21 armbuild/fedora-qcow-minimal:latest
-docker tag armbuild/fedora-qcow-minimal:21 armbuild/fedora-qcow-minimal:twenty-one
-docker tag armbuild/fedora-qcow-minimal:21 armbuild/fedora-qcow-minimal:21-5
+docker build -t armbuild/fedora-qcow-minimal:22 .
+docker tag armbuild/fedora-qcow-minimal:22 armbuild/fedora-qcow-minimal:latest
+docker tag armbuild/fedora-qcow-minimal:22 armbuild/fedora-qcow-minimal:twenty-two
+docker tag armbuild/fedora-qcow-minimal:22 armbuild/fedora-qcow-minimal:22-3
